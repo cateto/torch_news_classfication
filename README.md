d420f0ab2e0e48cb335d58182ceffcad5565cd36
cateto <u2skind@gmail.com>
2021-09-24 14:23:44 +0900

    20210924

---
+++ README.md
@@ -0,0 +1,75 @@
+
+
+# KoBERT-news-classfication
+
+- KoBERT를 이용한 언론 주제 분류(Document Classfication)
+
+
+
+#### Requirements
+
+- Python >= 3.6
+- PyTorch >= 1.7.0
+- MXNet >= 1.4.0
+- gluonnlp >= 0.6.0
+- sentencepiece >= 0.1.6
+- onnxruntime >= 0.3.0
+- transformers >= 3.5.0
+
+
+
+#### How to install
+
+```console
+pip install mxnet
+pip install gluonnlp pandas tqdm
+pip install sentencepiece
+pip install transformers==3
+pip install torch
+
+pip install git+https://git@github.com/SKTBrain/KoBERT.git@master
+```
+
+
+
+#### Prediction
+
+```console
+python main.py --input_file {INPUT_FILE_PATH} --model_dir {SAVED_CKPT_PATH}
+
+[for custom setting]
+
+OR
+
+python main.py
+
+[for default setting]
+model dir : './model/20210802/model.pt'
+input file : './data/predict_lucy.csv'
+```
+
+
+
+#### Results
+
+83% of accuracy
+
+
+
+#### Structure
+
+```console
+├── bert                       # python class for load bert model
+├── data             		   # data for predict
+├── model                      # pytorch pretrained model
+│   └── 20210802               
+├── notebook                   # all source code made by colab
+├── output                     # predicted data (csv)
+├── main.py                    
+├── predict.py
+├── README.md
+└── utils.py
+```
+
+
+
---
+++ main.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from predict import Predict
+from bert import BERTClassifier
+import os
+from utils import init_logger
+import argparse
+
+def predict_func(pred_config):
+  DATA_COL = 'contents'
+  LABEL_COL = 'category'
+  predict = Predict()
+  print(pred_config.input_file)
+  print(pred_config.model_dir)
+  tok, model = predict.load_model_n_tokenizer(pred_config.model_dir)
+  classfier = BERTClassifier(model)
+  lucy_data = predict.preprocess_data(pred_config.input_file, DATA_COL)
+  save_path = predict.category_encoding_n_save(lucy_data, './temp/', LABEL_COL)
+  predict_set = predict.load_data(save_path, 1,2)
+  result_set = predict.predict_n_save_result(model, predict_set, tok, lucy_data, DATA_COL, './output/')
+  predict.get_accuracy(model, predict_set, tok)
+  if os.path.isfile(save_path):
+      os.remove(save_path)
+
+def main():
+  print("========================================Done")
+
+
+if __name__ == '__main__':
+  init_logger()
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--model_dir", default='./model/20210802/model.pt', type=str, help="Path to load model")
+  parser.add_argument("--input_file", default='./data/predict_lucy.csv', type=str, help="Input file for prediction")
+  pred_config = parser.parse_args()
+  predict_func(pred_config)
+  main()
